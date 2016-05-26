bb794835f1b17859b341c9cf49e6f675605777ee
Casey Law <caseyjlaw@gmail.com>
2016-05-26 17:51:07 -0600

    working with fixed ip in berkeley. fixed bug for last cand

--- rflearn/espost_cands.py
+++ rflearn/espost_cands.py
@@ -1,62 +1,54 @@
-import pickle, json, requests, logging
+import pickle, json, requests, logging, os
 from rtpipe.parsecands import read_candidates
 
+logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def parse(candsfile, url='', onlyplots=False):
+def parse(candsfile, plotdir='/users/claw/public_html/plots', onlyplots=True):
     """ Get cands info into form ready to post 
 
-    optional url command gives location of candidate plots to filter parsed list
-    onlyplots says to filter by plots that exist at url
+    plotdir command gives location of candidate plots to filter parsed list
+    onlyplots says to filter by plots that exist in plotdir
     """
 
     loc, prop, state = read_candidates(candsfile, returnstate=True)
 
     fileroot = state['fileroot']
-    scancol = state['featureind'].index('scan')
-    segmentcol = state['featureind'].index('segment')
-    intcol = state['featureind'].index('int')
-    dmcol = state['featureind'].index('dmind')
-    dtcol = state['featureind'].index('dtind')
-    if 'snr2' in state['features']:
-        snrcol = state['features'].index('snr2')
-    elif 'snr1' in state['features']:
-        snrcol = state['features'].index('snr1')
 
     alldata = []
-
-    for i, key in enumerate(loc):
+    for i in range(len(loc)):
         data = {}
         data['obs'] = fileroot
-        data['scan'] = key[scancol]
-        data['segment'] = key[segmentcol]
-        data['integration'] = key[intcol]
-        data['dm'] = key[dmcol]
-        data['dt'] = key[dtcol]
-        value = prop[i]
-        data['snr'] = value[snrcol]
- 
-        uniqueid = '{0}_sc{1}-seg{2}-i{3}-dm{4}-dt{5}'.format(data['obs'], data['scan'], data['segment'], data['integration'], data['dm'], data['dt'])
+
+        for feat in state['featureind']:
+            col = state['featureind'].index(feat)
+            data[feat] = loc[i][col]
+
+        for feat in state['features']:
+            col = state['features'].index(feat)
+            data[feat] = prop[i][col]
+
+        uniqueid = '{0}_sc{1}-seg{2}-i{3}-dm{4}-dt{5}'.format(data['obs'], data['scan'], data['segment'], data['int'], data['dmind'], data['dtind'])
         data['candidate_png'] = 'cands_{0}.png'.format(uniqueid)
   
         idobj = {}
         idobj['_id'] = uniqueid
         if onlyplots:
-            if os.path.exists(os.path.join(url, data['candidate_png'])):
+            if os.path.exists(os.path.join(plotdir, data['candidate_png'])):
                 alldata.append({"index":idobj})
                 alldata.append(data)
             else:
                 logger.info('Plot not found for {0}. Not including.'.format(data['candidate_png']))
 
     jsonStr = json.dumps(alldata,separators=(',', ':'))
-    cleanjson = jsonStr.replace('}},','}}\n').replace('},','}\n').replace(']','').replace('[','')
+    cleanjson = jsonStr.replace('}},','}}\n').replace('},','}\n').replace(']','').replace('[','') + '\n'
 
     return cleanjson
 
 
-def post(cleanjson, url='http://localhost:9200/realfast/cands/_bulk?'):
+def post(cleanjson, url='http://136.152.227.149:9200/realfast/cand/_bulk?'):
     """ Post json to elasticsearch instance """
 
     r = requests.post(url, data=cleanjson)
